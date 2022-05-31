b6b38d6bcd8d0d0e72b01775e7502506a7397e86
ABULAFIA JEREMIAS <47297677@ort>
2022-05-31 08:40:03 -0300

    Initial commit

---
+++ Assets/PlaySoundOnSpace.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnSpace : MonoBehaviour
+{
+    public AudioClip aaa;
+    AudioSource fuenteAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuenteAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            fuenteAudio.clip = aaa;
+            fuenteAudio.Play();
+        }
+    }
+}
